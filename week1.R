9f58846e8def5e40e0b96c513489b44112ee71e7
KatherinePineault <katherine.pineault@gmail.com>
2016-10-26 09:48:31 -0400

    First commit

---
+++ week1.R
@@ -0,0 +1,17 @@
+# Developing Data Products - Week 1
+## Ce script contient l'apprentissage de base pour le package Shiny.
+
+#########################
+# Katherine Pineault
+# Début 25 oct. 2016
+# Fin 
+#########################
+
+# NOTE: A shiny project is a DIRECTORY containing at least two files: 
+     # ui.R (user interface) controls how your apps looks
+     # server.R that controls what your app does.
+     
+
+rm(list=ls()) # clear workspace
+
+library(shiny)
